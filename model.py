00ad6d6058b79408af8482635ad9bbe1daa9e1f0
KgasagoGH <kgasago.mash@gmail.com>
2023-11-16 00:39:39 +0200

    edited model.py

--- model.py
+++ model.py
@@ -30,10 +30,6 @@ import json
 def _preprocess_data(data):
     """Private helper function to preprocess data for model prediction.
 
-    NB: If you have utilised feature engineering/selection in order to create
-    your final model you will need to define the code here.
-
-
     Parameters
     ----------
     data : str
@@ -41,7 +37,7 @@ def _preprocess_data(data):
 
     Returns
     -------
-    Pandas DataFrame : <class 'pandas.core.frame.DataFrame'>
+    pd.DataFrame
         The preprocessed data, ready to be used our model for prediction.
     """
     # Convert the json string to a python dictionary object
@@ -49,33 +45,34 @@ def _preprocess_data(data):
     # Load the dictionary as a Pandas DataFrame.
     feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
 
-    # ---------------------------------------------------------------
-    # NOTE: You will need to swap the lines below for your own data
-    # preprocessing methods.
-    #
-    # The code below is for demonstration purposes only. You will not
-    # receive marks for submitting this code in an unchanged state.
-    # ---------------------------------------------------------------
+    # Specify numerical and categorical columns
+    numerical_columns = ['Madrid_wind_speed', 'Valencia_wind_deg', 'Bilbao_rain_1h', 'Valencia_wind_speed',
+                          'Seville_humidity', 'Madrid_humidity', 'Bilbao_clouds_all', 'Bilbao_wind_speed',
+                          'Seville_clouds_all', 'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg',
+                          'Madrid_clouds_all', 'Seville_wind_speed', 'Barcelona_rain_1h', 'Seville_pressure',
+                          'Seville_rain_1h', 'Bilbao_snow_3h', 'Barcelona_pressure', 'Seville_rain_3h',
+                          'Madrid_rain_1h', 'Barcelona_rain_3h', 'Valencia_snow_3h', 'Madrid_weather_id',
+                          'Barcelona_weather_id', 'Bilbao_pressure', 'Seville_weather_id', 'Valencia_pressure',
+                          'Seville_temp_max', 'Madrid_pressure', 'Valencia_temp_max', 'Valencia_temp',
+                          'Bilbao_weather_id', 'Seville_temp', 'Valencia_humidity', 'Valencia_temp_min',
+                          'Barcelona_temp_max', 'Madrid_temp_max', 'Barcelona_temp', 'Bilbao_temp_min',
+                          'Bilbao_temp', 'Barcelona_temp_min', 'Bilbao_temp_max', 'Seville_temp_min',
+                          'Madrid_temp', 'Madrid_temp_min']
 
-    # ----------- Replace this code with your own preprocessing steps --------
-    columns_to_select = ['Madrid_wind_speed', 'Valencia_wind_deg',
-       'Bilbao_rain_1h', 'Valencia_wind_speed', 'Seville_humidity',
-       'Madrid_humidity', 'Bilbao_clouds_all', 'Bilbao_wind_speed',
-       'Seville_clouds_all', 'Bilbao_wind_deg', 'Barcelona_wind_speed',
-       'Barcelona_wind_deg', 'Madrid_clouds_all', 'Seville_wind_speed',
-       'Barcelona_rain_1h', 'Seville_pressure', 'Seville_rain_1h',
-       'Bilbao_snow_3h', 'Barcelona_pressure', 'Seville_rain_3h',
-       'Madrid_rain_1h', 'Barcelona_rain_3h', 'Valencia_snow_3h',
-       'Madrid_weather_id', 'Barcelona_weather_id', 'Bilbao_pressure',
-       'Seville_weather_id', 'Valencia_pressure', 'Seville_temp_max',
-       'Madrid_pressure', 'Valencia_temp_max', 'Valencia_temp',
-       'Bilbao_weather_id', 'Seville_temp', 'Valencia_humidity',
-       'Valencia_temp_min', 'Barcelona_temp_max', 'Madrid_temp_max',
-       'Barcelona_temp', 'Bilbao_temp_min', 'Bilbao_temp',
-       'Barcelona_temp_min', 'Bilbao_temp_max', 'Seville_temp_min',
-       'Madrid_temp', 'Madrid_temp_min']
-    predict_vector = feature_vector_df[columns_to_select]
+    categorical_columns = ['Madrid_weather_id', 'Barcelona_weather_id', 'Bilbao_weather_id', 'Seville_weather_id']
 
+    # Ensure numerical columns are treated as numeric
+    feature_vector_df[numerical_columns] = feature_vector_df[numerical_columns].apply(pd.to_numeric, errors='coerce')
+
+    # Impute missing values in numerical columns with mean
+    feature_vector_df[numerical_columns] = feature_vector_df[numerical_columns].fillna(feature_vector_df[numerical_columns].mean())
+
+    # Handle categorical columns (you may need to encode them if required)
+    feature_vector_df[categorical_columns] = feature_vector_df[categorical_columns].astype('category')
+
+    # Add any additional preprocessing steps as needed
+
+    return feature_vector_df
     # ------------------------------------------------------------------------
 
     return predict_vector
