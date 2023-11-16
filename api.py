4206190cf3ab7d61708b540f422a1f5235991f8d
NINA-06 <mukovhenina@gmail.com>
2023-11-17 01:18:41 +0200

    changes

--- api.py
+++ api.py
@@ -31,7 +31,6 @@ app = Flask(__name__)
 # Please update this path to reflect your own trained model.
 static_model = load_model(
     path_to_model='assets/trained-models/best_model.pkl')
-    path_to_model='assets/trained-models/load_shortfall_simple_lm_regression.pkl')
 
 print ('-'*40)
 print ('Model successfully loaded')
@@ -64,5 +63,4 @@ def model_prediction():
 # This will allow Flask to automatically restart itself everytime you
 # update your API code.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False)
     app.run(host='0.0.0.0', port=5000, debug=False)
