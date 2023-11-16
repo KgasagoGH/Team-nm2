6e14a6f1dc95368e27fe0a1bcaf6062bf5982752
KgasagoGH <kgasago.mash@gmail.com>
2023-11-16 13:55:52 +0200

    j

--- utils/train_model.py
+++ utils/train_model.py
@@ -26,6 +26,6 @@ print ("Training Model...")
 lm_regression.fit(X_train, y_train)
 
 # Pickle model for use within our API
-save_path = '../assets/trained-models/basic_model.pkl'
+save_path = '../assets/trained-models/best_model.pkl'
 print (f"Training completed. Saving model to: {save_path}")
 pickle.dump(lm_regression, open(save_path,'wb'))
