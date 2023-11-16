c5738af51855da86f8da5a412e6556f5dc8cdf15
KgasagoGH <kgasago.mash@gmail.com>
2023-11-16 11:21:22 +0200

    changes

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ def _preprocess_data(data):
         The data payload received within POST requests sent to our API.
 
     Returns
-    ------- 
+    -------
     Pandas DataFrame : <class 'pandas.core.frame.DataFrame'>
         The preprocessed data, ready to be used our model for prediction.
     """
@@ -68,7 +68,9 @@ def _preprocess_data(data):
     'Valencia_temp','Bilbao_weather_id','Seville_temp','Valencia_humidity',
     'Valencia_temp_min','Barcelona_temp_max','Madrid_temp_max','Barcelona_temp',
     'Bilbao_temp_min','Bilbao_temp','Barcelona_temp_min','Bilbao_temp_max',
-    'Seville_temp_min','Madrid_temp','Madrid_temp_min']]
+    'Seville_temp_min','Madrid_temp','Madrid_temp_min','Day','Month','Year',
+    'Hour','Start_minute','start_seconds','Start_weekend','Start_week_of_year',
+    'winter','spring','summer','autumn']]
     # ------------------------------------------------------------------------
 
     return predict_vector
